841bf148354d19abd3c3db390ea87d4888c9c51f
Jordan <jordanbscott12@gmail.com>
2022-04-28 11:21:59 -0400

    Added my own configurations

--- config.lua
+++ config.lua
@@ -3,49 +3,53 @@ Config = {}
 Config.UseTarget = GetConvar('UseTarget', false)
 
 Config.MinimalDoctors = 2
-Config.WipeInventoryOnRespawn = true
+Config.WipeInventoryOnRespawn = false
 
 Config.Locations = {
     ["checking"] = {
-	    [1] = vector3(308.19, -595.35, 43.29),
-	    [2] = vector3(-254.54, 6331.78, 32.43), -- paleto
+	    [1] = vector3(309.09, -592.51, 43.28),
+        [2] = vector3(350.85, -588.11, 28.8),
+	    [3] = vector3(-254.54, 6331.78, 32.43), -- paleto
     },
     ["duty"] = {
-        [1] = vector3(311.18, -599.25, 43.29),
-        [2] = vector3(-254.88, 6324.5, 32.58),
+        [1] = vector3(310.63, -597.06, 43.28),
+        [2] = vector3(-653.73, -87.68, 38.79), -- fire
+        [3] = vector3(-254.88, 6324.5, 32.58), -- paleto
     },
     ["vehicle"] = {
-        [1] = vector4(294.578, -574.761, 43.179, 35.79),
-        [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
+        [1] = vector4(293.08, -582.62, 43.2, 344.87),
+        [2] = vector4(-234.28, 6329.16, 32.15, 222.5), -- paleto
     },
     ["helicopter"] = {
         [1] = vector4(351.58, -587.45, 74.16, 160.5),
-        [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
+        [2] = vector4(-475.43, 5988.353, 31.716, 31.34), -- paleto
     },
     ["armory"] = {
-        [1] = vector3(309.93, -602.94, 43.29),
-        [2] = vector3(-245.13, 6315.71, 32.82),
+        [1] = vector3(306.63, -601.76, 43.28),
+        [2] = vector3(-623.88, -109.03, 45.5), -- fire
+        [3] = vector3(-245.13, 6315.71, 32.82), -- paleto
     },
     ["roof"] = {
         [1] = vector4(338.5, -583.85, 74.16, 245.5),
     },
     ["main"] = {
-        [1] = vector3(298.74, -599.33, 43.29),
+        [1] = vector3(327.05, -603.75, 43.28),
     },
     ["stash"] = {
-        [1] = vector3(309.78, -596.6, 43.29),
+        [1] = vector3(302.06, -599.31, 43.28),
+        [2] = vector3(-629.2, -85.22, 45.41),
     },
     ["beds"] = {
-        [1] = {coords = vector4(353.1, -584.6, 43.11, 152.08), taken = false, model = 1631638868},
-        [2] = {coords = vector4(356.79, -585.86, 43.11, 152.08), taken = false, model = 1631638868},
-        [3] = {coords = vector4(354.12, -593.12, 43.1, 336.32), taken = false, model = 2117668672},
-        [4] = {coords = vector4(350.79, -591.8, 43.1, 336.32), taken = false, model = 2117668672},
-        [5] = {coords = vector4(346.99, -590.48, 43.1, 336.32), taken = false, model = 2117668672},
-        [6] = {coords = vector4(360.32, -587.19, 43.02, 152.08), taken = false, model = -1091386327},
-        [7] = {coords = vector4(349.82, -583.33, 43.02, 152.08), taken = false, model = -1091386327},
-        [8] = {coords = vector4(326.98, -576.17, 43.02, 152.08), taken = false, model = -1091386327},
-	--- paleto
-	    [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
+        [1] = {coords = vector4(322.72, -587.08, 43.02, 343.95), taken = false, model = 1631638868},
+        [2] = {coords = vector4(324.2, -582.85, 43.02, 158.67), taken = false, model = 1631638868},
+        [3] = {coords = vector4(317.74, -585.55, 43.02, 337.88), taken = false, model = 2117668672},
+        [4] = {coords = vector4(319.35, -580.93, 43.02, 157.94), taken = false, model = 2117668672},
+        [5] = {coords = vector4(313.94, -578.93, 43.02, 156.02), taken = false, model = 2117668672},
+        [6] = {coords = vector4(311.11, -583.14, 43.02, 336.57), taken = false, model = -1091386327},
+        [7] = {coords = vector4(309.26, -577.33, 43.02, 156.31), taken = false, model = -1091386327},
+        [8] = {coords = vector4(307.82, -581.8, 43.02, 336.44), taken = false, model = -1091386327},
+        --- paleto
+        [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
         [10] = {coords = vector4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672},
         [11] = {coords = vector4(-255.98, 6315.67, 32.34, 313.91), taken = false, model = 2117668672},
     },
@@ -58,23 +62,41 @@ Config.AuthorizedVehicles = {
 	-- Grade 0
 	[0] = {
 		["ambulance"] = "Ambulance",
+        ["f550ambo"] = "Ford F250 Ambulance",
 	},
 	-- Grade 1
 	[1] = {
 		["ambulance"] = "Ambulance",
-
+        ["flyhoe"] = "Chevy Tahoe",
+        ["f550ambo"] = "Ford F250 Ambulance",
+        ["firetruck"] = "Firetruck",
 	},
 	-- Grade 2
 	[2] = {
 		["ambulance"] = "Ambulance",
+        ["flyhoe"] = "Chevy Tahoe",
+        ["fsilv"] = "Chevy Silverado",
+        ["f550ambo"] = "Ford F250 Ambulance",
+        ["firetruck"] = "Firetruck",
 	},
 	-- Grade 3
 	[3] = {
 		["ambulance"] = "Ambulance",
+        ["flyhoe"] = "Chevy Tahoe",
+        ["FD2"] = "Dodge Charger",
+        ["fsilv"] = "Chevy Silverado",
+        ["f550ambo"] = "Ford F250 Ambulance",
+        ["firetruck"] = "Firetruck",
 	},
 	-- Grade 4
 	[4] = {
 		["ambulance"] = "Ambulance",
+        ["flyhoe"] = "Chevy Tahoe",
+        ["spartan"] = "Big Bertha",
+        ["FD2"] = "Dodge Charger",
+        ["fsilv"] = "Chevy Silverado",
+        ["f550ambo"] = "Ford F250 Ambulance",
+        ["firetruck"] = "Firetruck",
 	}
 }
 
@@ -132,11 +154,43 @@ Config.Items = {
             type = "item",
             slot = 6,
         },
+        [7] = {
+            name = "advancedrepairkit",
+            price = 0,
+            amount = 10,
+            info = {},
+            type = "item",
+            slot = 6,
+        },
+        [8] = {
+            name = "ifaks",
+            price = 0,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 7,
+        },
+        [9] = {
+            name = "weapon_hatchet",
+            price = 0,
+            amount = 1,
+            info = {},
+            type = "item",
+            slot = 8,
+        },
+        [10] = {
+            name = "notepad",
+            price = 0,
+            amount = 1,
+            info = {},
+            type = "item",
+            slot = 9,
+        },
     }
 }
 
-Config.BillCost = 2000
-Config.DeathTime = 300
+Config.BillCost = 1000
+Config.DeathTime = 150
 Config.CheckTime = 10
 
 Config.PainkillerInterval = 60 -- seconds
